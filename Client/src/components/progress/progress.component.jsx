29c7929e14ded84918e2456021aef241ff9ddaac
ahamideh <ahamideh18@hotmail.com>
2021-06-20 16:15:00 -0400

    Removed unused packages. Made custom text progress indicator using framer-motion and tailwind styles instead of the 3rd part library

--- Client/src/components/progress/progress.component.jsx
+++ Client/src/components/progress/progress.component.jsx
@@ -1,18 +1,31 @@
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import { motion, useSpring, useTransform, useMotionValue } from "framer-motion"
 
 const ProgressCircle = ({ progress, maxValue, text }) => {
+    const initialValue = useMotionValue(0)
+    const progressPercent = useTransform(initialValue, () => progress / maxValue);
+    const pathLength = useSpring(progressPercent, { stiffness: 400, damping: 90 });
+
     return (
-        <div className="text-progress-circle mt-3" style={{ width: 50, height: 50 }}>
-            <CircularProgressbar value={progress} maxValue={maxValue} text={text}
-                styles={buildStyles({
-                    pathTransitionDuration: 0.1,
-                    textColor: progress <= maxValue ? `rgb(0, 0, 0)` : `rgb(207, 43, 43)`,
-                    pathColor: progress <= maxValue ? `rgba(79, 70, 229, ${progress / maxValue})` : `rgb(207, 43, 43)`,
-                    trailColor: '#d6d6d6',
-                })}
+        <svg className="flex justify-center align-center mt-3" width="50px" height="50px">
+            <motion.path
+                className="justify-self-center"
+                fill="none"
+                strokeWidth="5"
+                stroke={progress <= maxValue ? `rgba(79, 70, 229, ${progress / maxValue})` : `rgb(207, 43, 43)`}
+                strokeDasharray="0 1"
+                d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
+                style={{
+                    pathLength,
+                    rotate: 90,
+                    translateX: 5,
+                    translateY: 5,
+                    scaleX: -1
+                }}
             />
-        </div>
+            <text x="50%" y="48%" textAnchor="middle" dominantBaseline="central" style={{ fontSize: '8px' }} fill={progress <= maxValue ? `black` : `rgb(207, 43, 43)`}>
+                {text}
+            </text>
+        </svg>
     )
 }
 
